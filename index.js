97a59f3db22110745e461241da1e87ce483bfc77
jhilsman <jhilsman@gmail.com>
2014-05-02 22:53:24 -0400

    set port by $port

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 var Moniker = require('moniker');
-var port = 3250;
+//var port = 3250;
+var port=$port
 
 app.listen(port);
 
